8d2219466e3dab652681796983ea072eadf6a157
Kerri Lee <t-kerlee@microsoft.com>
2020-07-16 12:12:10 -0700

    incomplete consumer

---
+++ sdk/eventgrid/Azure.Messaging.EventGrid/src/Customization/EventGridConsumer.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Messaging.EventGrid.Models;
+
+namespace Azure.Messaging.EventGrid
+{
+    /// <summary>
+    /// Class used to decode events from the Event Grid service.
+    /// </summary>
+    public class EventGridConsumer
+    {
+        private readonly ObjectSerializer _objectSerializer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventGridConsumer"/> class.
+        /// </summary>
+        public EventGridConsumer()
+        {
+            // Initialize some sort of dictionary for custom event types
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in the EventGrid event schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in the EventGrid schema.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of EventGrid Events.</returns>
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public virtual async Task<EventGridEvent[]> DeserializeEventGridEventsAsync(string requestContent, CancellationToken cancellationToken = default)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in the EventGrid event schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in the EventGrid event schema.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of EventGrid Events.</returns>
+        public virtual Response<EventGridEvent[]> DeserializeEventGridEvents(string requestContent, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+            // use _objectSerializer to deserialize into list of eg events
+            // need to check if events are actually encoded in the eg schema
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in the CloudEvent schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in the CloudEvent schema.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of CloudEvents.</returns>
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public virtual async Task<CloudEvent[]> DeserializeCloudEventsAsync(string requestContent, CancellationToken cancellationToken = default)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in the CloudEvent schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in the CloudEvent schema.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of CloudEvents.</returns>
+        public virtual Response<CloudEvent[]> DeserializeCloudEvents(string requestContent, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+            // use _objectSerializer to deserialize into list of cloud events
+            // need to check if events are actually encoded in the cloudevent schema
+        }
+    }
+}
