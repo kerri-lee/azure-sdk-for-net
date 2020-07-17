ec1955648d830d86872be9fd74b03fe75a9ac90d
Kerri Lee <t-kerlee@microsoft.com>
2020-07-17 14:37:36 -0700

    generated deserialization methods using swagger

--- sdk/eventgrid/Azure.Messaging.EventGrid/src/Customization/EventGridConsumer.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid/src/Customization/EventGridConsumer.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
@@ -17,6 +19,7 @@ namespace Azure.Messaging.EventGrid
     public class EventGridConsumer
     {
         private readonly ObjectSerializer _objectSerializer;
+        private readonly ConcurrentDictionary<string, Type> _customEventTypeMappings;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EventGridConsumer"/> class.
@@ -24,6 +27,11 @@ namespace Azure.Messaging.EventGrid
         public EventGridConsumer()
         {
             // Initialize some sort of dictionary for custom event types
+            _customEventTypeMappings = new ConcurrentDictionary<string, Type>();
+            _objectSerializer = new JsonObjectSerializer(new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
         }
 
         /// <summary>
@@ -49,7 +57,7 @@ namespace Azure.Messaging.EventGrid
         /// </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <returns>A list of EventGrid Events.</returns>
-        public virtual Response<EventGridEvent[]> DeserializeEventGridEvents(string requestContent, CancellationToken cancellationToken = default)
+        public virtual EventGridEvent[] DeserializeEventGridEvents(string requestContent, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
             // use _objectSerializer to deserialize into list of eg events
@@ -79,11 +87,112 @@ namespace Azure.Messaging.EventGrid
         /// </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <returns>A list of CloudEvents.</returns>
-        public virtual Response<CloudEvent[]> DeserializeCloudEvents(string requestContent, CancellationToken cancellationToken = default)
+        public virtual CloudEvent[] DeserializeCloudEvents(string requestContent, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
             // use _objectSerializer to deserialize into list of cloud events
             // need to check if events are actually encoded in the cloudevent schema
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in a custom event schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in a custom event schema.
+        /// </param>
+        /// <param name="customEventType">
+        /// Custom event type that <paramref name="requestContent"/> will be decoded into.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of custom events.</returns>
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public virtual async Task<object[]> DeserializeCustomEventsAsync(string requestContent, Type customEventType, CancellationToken cancellationToken = default)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deserializes JSON encoded events and returns an array of events encoded in a custom event schema.
+        /// </summary>
+        /// <param name="requestContent">
+        /// The JSON encoded representation of either a single event or an array or events, encoded in a custom event schema.
+        /// </param>
+        /// <param name="customEventType">
+        /// Custom event type that <paramref name="requestContent"/> will be decoded into.
+        /// </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns>A list of CloudEvents.</returns>
+        public virtual object[] DeserializeCustomEvents(string requestContent, Type customEventType, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+            // use _objectSerializer to deserialize into list of custom events
+            // need to check if events are actually encoded in the custom event schema
+        }
+
+        /// <summary>
+        /// Adds or updates a custom event mapping that associates an eventType string with the corresponding type of event data.
+        /// </summary>
+        /// <param name="eventType">The event type to register, such as "Contoso.Items.ItemReceived"</param>
+        /// <param name="eventDataType">The type of eventdata corresponding to this eventType, such as typeof(ContosoItemReceivedEventData)</param>
+        public void AddOrUpdateCustomEventMapping(string eventType, Type eventDataType)
+        {
+            ValidateEventType(eventType);
+
+            if (eventDataType == null)
+            {
+                throw new ArgumentNullException(nameof(eventDataType));
+            }
+
+            _customEventTypeMappings.AddOrUpdate(
+                eventType,
+                eventDataType,
+                (_, existingValue) => eventDataType);
+        }
+
+        /// <summary>
+        /// Gets information about a custom event mapping.
+        /// </summary>
+        /// <param name="eventType">The registered event type, such as "Contoso.Items.ItemReceived"</param>
+        /// <param name="eventDataType">The type of eventdata corresponding to this eventType, such as typeof(ContosoItemReceivedEventData)</param>
+        /// <returns>True if the specified mapping exists.</returns>
+        public bool TryGetCustomEventMapping(string eventType, out Type eventDataType)
+        {
+            ValidateEventType(eventType);
+
+            return _customEventTypeMappings.TryGetValue(eventType, out eventDataType);
+        }
+
+        /// <summary>
+        /// List all registered custom event mappings.
+        /// </summary>
+        /// <returns>An IEnumerable of mappings</returns>
+        public IEnumerable<KeyValuePair<string, Type>> ListAllCustomEventMappings()
+        {
+            foreach (KeyValuePair<string, Type> kvp in _customEventTypeMappings)
+            {
+                yield return kvp;
+            }
+        }
+
+        /// <summary>
+        /// Removes a custom event mapping.
+        /// </summary>
+        /// <param name="eventType">The registered event type, such as "Contoso.Items.ItemReceived"</param>
+        /// <param name="eventDataType">The type of eventdata corresponding to this eventType, such as typeof(ContosoItemReceivedEventData)</param>
+        /// <returns>True if the specified mapping was removed successfully.</returns>
+        public bool TryRemoveCustomEventMapping(string eventType, out Type eventDataType)
+        {
+            ValidateEventType(eventType);
+            return _customEventTypeMappings.TryRemove(eventType, out eventDataType);
+        }
+
+        internal void ValidateEventType(string eventType)
+        {
+            if (string.IsNullOrEmpty(eventType))
+            {
+                throw new ArgumentNullException(nameof(eventType));
+            }
         }
     }
 }
---
+++ sdk/eventgrid/Azure.Messaging.EventGrid/tests/ConsumeEventTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Messaging.EventGrid.Tests
+{
+    public class ConsumeEventTests
+    {
+        public readonly EventGridConsumer _eventGridConsumer;
+
+        public ConsumeEventTests()
+        {
+            _eventGridConsumer = new EventGridConsumer();
+        }
+    }
+}
